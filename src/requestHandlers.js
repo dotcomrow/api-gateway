3acdec55033336e559953bf1f17633afdf78c023
mephmanx <52037738+mephmanx@users.noreply.github.com>
2024-07-20 00:59:52 +0000

    Deploying to release from @ dotcomrow/api-gateway@9bfa111c57c50a7ecc75a715e4e2d0e4897d348f 🚀

---
+++ src/requestHandlers.js
@@ -0,0 +1,15 @@
+export async function handleDelete(env, account_id, query, itemId) {
+  return {};
+}
+
+export async function handlePost(env, account_id, body) {
+  return {};
+}
+
+export async function handleGet(env, account_id, query, itemId) {
+  return {};
+}
+
+export async function handlePut(env, account_id, body) {
+  return {};
+}
