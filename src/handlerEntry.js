37de2109f95ce2ffa3c5ef0072f6bb6d78ea8518
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-08-17 16:47:32 -0400

    Dev (#8)
    * refactoring to allow unauthenticated requests
    
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>
    
    * whoops
    
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- src/handlerEntry.js
+++ src/handlerEntry.js
@@ -5,6 +5,7 @@ import { jsonb, numeric, varchar } from "drizzle-orm/pg-core";
 import { drizzle } from "drizzle-orm/d1";
 import { eq } from "drizzle-orm";
 import { init_script } from "./init_script.js";
+import { default as AuthenticationUtility } from "./utility/AuthenticationUtility.js";
 
 export async function handleRequest(request, env, context) {
   var origin = request.headers.get("Origin") || request.headers.get("origin");
@@ -42,139 +43,110 @@ export async function handleRequest(request, env, context) {
     });
   }
 
-  var authHeader = "";
-  if (
-    request.headers.get("Authorization") != undefined ||
-    request.headers.get("authorization") != undefined
-  ) {
-    if (
-      request.headers.get("Authorization") != undefined &&
-      request.headers.get("Authorization").startsWith("Bearer ")
-    ) {
-      authHeader = request.headers.get("Authorization").split(" ")[1];
-    } else if (
-      request.headers.get("authorization") != undefined &&
-      request.headers.get("authorization").startsWith("Bearer ")
-    ) {
-      authHeader = request.headers.get("authorization").split(" ")[1];
-    }
-  } else {
-    return new Response(
-      JSON.stringify({ message: "Authorization header not found." }),
-      {
-        status: 403,
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
+  var accountResponse = undefined;
+  if (request.headers.get("Authorization"))
+    accountResponse = await AuthenticationUtility.fetchAccountInfo(
+      request.headers.get("Authorization").split(" ")[1]
     );
-  }
-
-  const googleProfileUrl =
-    "https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=" +
-    authHeader;
-
-  var response = await fetch(googleProfileUrl, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-
-  var accountResponse = JSON.parse(await response.text());
-  if (accountResponse == undefined || accountResponse["id"] == undefined) {
-    return new Response(
-      JSON.stringify({ message: "Account not found / token invalid." }),
-      {
-        status: 403,
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
-  }
 
   var request_headers = {};
   for (var entry of request.headers.entries()) {
     request_headers[entry[0]] = entry[1];
   }
-  request_headers["X-Auth-User"] = accountResponse["id"];
-  request_headers["X-Auth-Email"] = accountResponse["email"];
-  request_headers["X-Auth-Name"] = accountResponse["name"];
-  request_headers["X-Auth-Profile"] = accountResponse["picture"];
-  request_headers["X-Auth-Provider"] = "google";
   request_headers["X-Shared-Secret"] = await env.GLOBAL_SHARED_SECRET;
 
-  const db = drizzle(env.cache);
-  const cache = sqliteTable("cache", {
-    account_id: varchar("account_id").notNull().primaryKey(),
-    response: jsonb("response").notNull(),
-    last_update_datetime: numeric("last_update_datetime").notNull(),
-  });
+  if (accountResponse != undefined) {
+    request_headers["X-Auth-User"] = accountResponse["id"];
+    request_headers["X-Auth-Email"] = accountResponse["email"];
+    request_headers["X-Auth-Name"] = accountResponse["name"];
+    request_headers["X-Auth-Profile"] = accountResponse["picture"];
+    request_headers["X-Auth-Provider"] = "google";
+    
+    const db = drizzle(env.cache);
+    const cache = sqliteTable("cache", {
+      account_id: varchar("account_id").notNull().primaryKey(),
+      response: jsonb("response").notNull(),
+      last_update_datetime: numeric("last_update_datetime").notNull(),
+    });
 
-  try {
-    var res = await db
-      .select()
-      .from(cache)
-      .where(eq(cache.account_id, accountResponse["id"]));
-  } catch (error) {
-    await init_script(env);
-    var res = await db
-      .select()
-      .from(cache)
-      .where(eq(cache.account_id, accountResponse["id"]));
-  }
-  if (res.length == 0) {
-    var userinfo_token = new GCPAccessToken(
-      env.GCP_USERINFO_CREDENTIALS
-    ).getAccessToken(
-      "https://www.googleapis.com/auth/admin.directory.group.readonly"
-    );
-    var userinfo_response = await GCPUserInfo.getUserInfo(
-      (
-        await userinfo_token
-      ).access_token,
-      accountResponse["id"],
-      env.DOMAIN
-    );
+    try {
+      var res = await db
+        .select()
+        .from(cache)
+        .where(eq(cache.account_id, accountResponse["id"]));
+    } catch (error) {
+      await init_script(env);
+      var res = await db
+        .select()
+        .from(cache)
+        .where(eq(cache.account_id, accountResponse["id"]));
+    }
+    if (res.length == 0) {
+      var userinfo_token = new GCPAccessToken(
+        env.GCP_USERINFO_CREDENTIALS
+      ).getAccessToken(
+        "https://www.googleapis.com/auth/admin.directory.group.readonly"
+      );
+      var userinfo_response = await GCPUserInfo.getUserInfo(
+        (
+          await userinfo_token
+        ).access_token,
+        accountResponse["id"],
+        env.DOMAIN
+      );
 
-    if (userinfo_response) {
-      // filter group data from response
-      var groups_return = [];
-      for (var obj of userinfo_response.groups) {
-        groups_return.push({
-          email: obj.email,
-          description: obj.description,
-        });
+      if (userinfo_response) {
+        // filter group data from response
+        var groups_return = [];
+        for (var obj of userinfo_response.groups) {
+          groups_return.push({
+            email: obj.email,
+            description: obj.description,
+          });
+        }
+        request_headers["X-Auth-Groups"] = JSON.stringify(groups_return);
       }
-      request_headers["X-Auth-Groups"] = JSON.stringify(groups_return);
+      await db
+        .insert(cache)
+        .values({
+          account_id: accountResponse["id"],
+          response: groups_return,
+          last_update_datetime: new Date().getTime(),
+        })
+        .execute();
+    } else {
+      if ((await env.SETTINGS.get("CACHE_EXPIRY")) == undefined) {
+        await env.SETTINGS.put("CACHE_EXPIRY", 5 * 60); // 5 minutes, cache in seconds
+      }
+      if (
+        res[0].last_update_datetime <
+        new Date().getTime() - (await env.SETTINGS.get("CACHE_EXPIRY")) * 1000
+      ) {
+        await db
+          .delete(cache)
+          .where(eq(cache.account_id, accountResponse["id"]))
+          .execute();
+      }
+      request_headers["X-Auth-Groups"] = JSON.stringify(res[0].response);
     }
-    await db
-      .insert(cache)
-      .values({
-        account_id: accountResponse["id"],
-        response: groups_return,
-        last_update_datetime: new Date().getTime(),
-      })
-      .execute();
-  } else {
-    if (await env.SETTINGS.get("CACHE_EXPIRY") == undefined) {
-      await env.SETTINGS.put("CACHE_EXPIRY", 5 * 60); // 5 minutes, cache in seconds
-    }
-    if (res[0].last_update_datetime < new Date().getTime() - await env.SETTINGS.get("CACHE_EXPIRY") * 1000) {
-      await db.delete(cache).where(eq(cache.account_id, accountResponse["id"])).execute();
-    }
-    request_headers["X-Auth-Groups"] = JSON.stringify(res[0].response);
   }
 
   var req_url = new URL(request.url);
-  
+
   var response = await fetch(
-    new Request("https://" + req_url.pathname.split("/")[1] + "." + env.ENVIRONMENT + "." + env.DOMAIN, {
-      method: request.method,
-      body: request.body,
-      headers: request_headers,
-    })
+    new Request(
+      "https://" +
+        req_url.pathname.split("/")[1] +
+        "." +
+        env.ENVIRONMENT +
+        "." +
+        env.DOMAIN,
+      {
+        method: request.method,
+        body: request.body,
+        headers: request_headers,
+      }
+    )
   );
 
   var response_headers = {};
