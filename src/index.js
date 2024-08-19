888040cdb83484ddb7ca7f21adcca5965e274186
mephmanx <52037738+mephmanx@users.noreply.github.com>
2024-08-19 00:04:07 +0000

    Deploying to release from @ dotcomrow/api-gateway@8fe078c8d79a51299ce60a3265a62ee7a1d72ab3 🚀

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import { handleRequest } from "./handlerEntry.js";
+import { serializeError } from "serialize-error";
+import { default as LogUtility } from "./utils/LoggingUtility.js";
+
+export default {
+  async fetch(request, env, context) {
+    self.location = new URL("https://www.google.com");
+    var loggingContext = LogUtility.buildLogContext(env);
+    try {
+      return await handleRequest(request, env, context, loggingContext);
+    } catch (e) {
+      const responseError = serializeError(e);
+      await LogUtility.logEntry(loggingContext, [
+        {
+          severity: "ERROR",
+          jsonPayload: {
+            message: "Exception occurred in fetch",
+            error: serializeError(err),
+          },
+        },
+      ]);
+      return new Response(JSON.stringify(responseError), {
+        status: 500,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+    }
+  },
+};
