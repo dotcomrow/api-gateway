e135cd68c485ef4ac1d7c6d1ced6405737fe9b91
mephmanx <52037738+mephmanx@users.noreply.github.com>
2024-08-19 00:11:16 +0000

    Deploying to release from @ dotcomrow/api-gateway@0d149cb23bf6d07df41a40b6eeb5a77911ceba77 🚀

---
+++ src/init_script.js
@@ -0,0 +1,25 @@
+import { serializeError } from "serialize-error";
+import { default as LogUtility } from "./utils/LoggingUtility.js";
+
+export async function init_script(env) {
+  try {
+    await env.cache
+      .prepare(
+        `CREATE TABLE cache (
+      account_id varchar(64) PRIMARY KEY,
+      response jsonb,
+      last_update_datetime numeric)`
+      )
+      .run();
+  } catch (e) {
+    await LogUtility.logEntry(LogUtility.buildLogContext(env), [
+      {
+        severity: "ERROR",
+        jsonPayload: {
+          message: "Exception occurred in fetch",
+          error: serializeError(err),
+        },
+      },
+    ]);
+  }
+}
