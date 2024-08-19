ce1b8e1e9634b2e16943e5faeaa88bd3fc516bec
mephmanx <52037738+mephmanx@users.noreply.github.com>
2024-08-19 12:41:34 +0000

    Deploying to release from @ dotcomrow/api-gateway@ef0874e5ecd709a6e931cc4477fbd8b5b2947712 🚀

---
+++ src/utils/LoggingUtility.js
@@ -0,0 +1,33 @@
+import { GCPLogger } from "npm-gcp-logging";
+import { v4 as uuidv4 } from "uuid";
+import { GCPAccessToken } from "npm-gcp-token";
+
+export default {
+  async buildLogContext(env) {
+    var context = {
+      ENVIRONMENT: env.ENVIRONMENT,
+      GCP_LOGGING_PROJECT_ID: env.GCP_LOGGING_PROJECT_ID,
+      LOGGING_TOKEN: await new GCPAccessToken(env.GCP_LOGGING_CREDENTIALS).getAccessToken("https://www.googleapis.com/auth/logging.write"),
+      LOG_NAME: env.LOG_NAME,
+      SpanId: uuidv4(),
+    }
+    return context;
+  },
+  async logEntry(context, entries) {
+    var finalEntries = [];
+    for (var entry of entries) {
+      entry.spanId = context.SpanId;
+      entry.labels = {
+        environment: context.ENVIRONMENT,
+        spanId: context.SpanId,
+      }
+      finalEntries.push(entry);
+    }
+    await GCPLogger.logEntry(
+      context.GCP_LOGGING_PROJECT_ID,
+      context.LOGGING_TOKEN,
+      context.LOG_NAME,
+      finalEntries
+    );
+  }
+}
